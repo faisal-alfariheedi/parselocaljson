e8e5f3ff44d3325d826a12c6968aaa55e2b9cb00
Faisal <falfariheedi@gmail.com>
2021-10-19 18:00:07 +0300

    a

--- app/src/main/java/com/example/parselocaljson/MainActivity.kt
+++ app/src/main/java/com/example/parselocaljson/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.parselocaljson
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RawRes
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var myJson = this.jsonToClass<cid>(R.raw.citylist)
     }
+    inline fun <reified T> Context.jsonToClass(@RawRes resourceId: Int): T =
+        Gson().fromJson(resources.openRawResource(resourceId).bufferedReader().use { it.readText() }, T::class.java)
 }
